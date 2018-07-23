a53c113ff35bd9b435779fa19bbd80d2514bd63c
SU VO <1512468@student.hcmus.edu.vn>
2018-07-23 23:24:25 +0700

    update, edit information user in database

--- HomeShoppe/HomeShoppe/Areas/Admin/Controllers/UserController.cs
+++ HomeShoppe/HomeShoppe/Areas/Admin/Controllers/UserController.cs
@@ -5,24 +5,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PagedList.Mvc;
 using System.Web.Mvc;
 
 namespace HomeShoppe.Areas.Admin.Controllers
 {
     public class UserController : BaseController
     {
-        // GET: Admin/User
-        public ActionResult Index()
+        #region method
+        public ActionResult Index(int page = 1, int sizePage = 10)
         {
-            return View();
+            var dao = new UserDAO();
+            var model = dao.ListAllPaging(page, sizePage);
+            return View(model);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var user = new UserDAO().ViewDetail(id);
+            return View(user);
+        }
+        #endregion
+
+        #region httpGet
         [HttpGet]
         public ActionResult Create()
         {
             return View();
         }
+        #endregion
 
+        #region httpPost
         [HttpPost]
         public ActionResult Create(User user)
         {
@@ -41,7 +54,34 @@ namespace HomeShoppe.Areas.Admin.Controllers
                     ModelState.AddModelError("", "Thêm mới user thành công");
                 }
             }
-            return View("Index");   
+            return View("Index");
         }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+       {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDAO();
+                if (!string.IsNullOrEmpty(user.Password))
+                {
+                    var encrytorMD5Pass = Encrytor.MD5Hash(user.Password);
+                    user.Password = encrytorMD5Pass;
+                }
+                
+                var  result= dao.Update(user);
+                if (result)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật user thành công");
+                }
+            }
+            return View("Index");
+        }
+        #endregion
+
     }
 }
--- HomeShoppe/Model/DAO/UserDAO.cs
+++ HomeShoppe/Model/DAO/UserDAO.cs
@@ -3,28 +3,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PagedList;
 using System.Threading.Tasks;
+
 
 namespace Model.DAO
 {
     public class UserDAO
     {
         HomeShoppeDB db = null;
+        #region contructor
         public UserDAO()
         {
             db = new HomeShoppeDB();
         }
 
+        public IEnumerable<User> ListAllPaging(int page, int pageSize)
+        {
+            return db.Users.OrderByDescending(x => x.CreateDate).ToPagedList(page, pageSize);
+        }
+
+        public User GetByID(string userName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+
+        public User ViewDetail(int id)
+        {
+            return db.Users.Find(id);
+        }
+        #endregion
+
+        #region usermethod
         public long Insert(User entity)
         {
             db.Users.Add(entity);
             db.SaveChanges();
             return entity.ID;
         }
-
-        public User GetByID(string userName)
+       
+        public bool Update(User entity)
         {
-            return db.Users.SingleOrDefault(x => x.UserName == userName);
+            try
+            {
+                var user = db.Users.Find(entity.ID);
+                user.UserName = entity.UserName;
+                if (!string.IsNullOrEmpty(user.Password))
+                {
+                    user.Password = entity.Password;
+                }
+                user.Address = entity.Address;
+                user.Email = entity.Email;
+                user.ModifiedBy = entity.ModifiedBy;
+                user.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // logging
+                return false;
+            }
         }
 
         public int Login(string userName, string passWord)
@@ -49,5 +88,12 @@ namespace Model.DAO
                 }
             }
         }
+        #endregion
+       
+
+        
+        
+
+        
     }
 }
