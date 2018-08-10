84c504feb1abe623efe0ba7da93362d55184a893
SU VO <1512468@student.hcmus.edu.vn>
2018-08-10 22:55:50 +0700

    add page index

--- HomeShoppe/HomeShoppe/Controllers/ProductController.cs
+++ HomeShoppe/HomeShoppe/Controllers/ProductController.cs
@@ -23,10 +23,28 @@ namespace HomeShoppe.Controllers
             return PartialView(model);
         }
 
-        public ActionResult Category(long cateId)
+        public ActionResult Category(long cateId, int page = 1, int pageSize = 1)
         {
             var category = new CategoryDAO().ViewDetail(cateId);
-            return View(category);
+            ViewBag.Category = category;
+            int totalRecord = 0;
+            var model = new ProductDAO().ListByCategoryID(cateId, ref totalRecord, page, pageSize);
+
+            ViewBag.Total = totalRecord;
+            ViewBag.Page = page;
+
+            int maxPage = 5;
+            int totalPage = 0;
+
+            totalPage = (int)Math.Ceiling((double)(totalRecord / pageSize));
+            ViewBag.TotalPage = totalPage;
+            ViewBag.MaxPage = maxPage;
+            ViewBag.First = 1;
+            ViewBag.Last = totalPage;
+            ViewBag.Next = page + 1;
+            ViewBag.Prev = page - 1;
+
+            return View(model);
         }
 
         public ActionResult Detail(long id)
--- HomeShoppe/Model/DAO/ProductDAO.cs
+++ HomeShoppe/Model/DAO/ProductDAO.cs
@@ -32,6 +32,12 @@ namespace Model.DAO
             return db.Products.Where(x => x.ID != productId && x.CategoryID==product.CategoryID).ToList();
         }
 
+        public List<Product> ListByCategoryID(long categoryID, ref int totalRecord, int pageIndex =1, int pageSize = 1)
+        {
+            totalRecord = db.Products.Where(x => x.CategoryID == categoryID).Count();
+            var model = db.Products.Where(x => x.CategoryID == categoryID).OrderByDescending(x=>x.CreateDate).Skip((pageIndex - 1)* pageSize).Take(pageSize).ToList();
+            return model;
+        }
         public Product ViewDetail(long id) 
         {
             return db.Products.Find(id);
