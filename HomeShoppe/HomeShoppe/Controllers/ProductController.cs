3115c652b0e95f0357ef3063d48e1081e60c7156
SU VO <1512468@student.hcmus.edu.vn>
2018-09-14 00:00:56 +0700

    add auto complete

--- HomeShoppe/HomeShoppe/Controllers/ProductController.cs
+++ HomeShoppe/HomeShoppe/Controllers/ProductController.cs
@@ -25,6 +25,17 @@ namespace HomeShoppe.Controllers
             return PartialView(model);
         }
 
+
+        public JsonResult ListName(string q)
+        {
+            var data = new ProductDAO().ListName(q);
+            return Json(new
+            {
+                data = data,
+                status = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Category(long cateId, int page = 1, int pageSize = 1)
         {
             var category = new CategoryDAO().ViewDetail(cateId);
@@ -35,6 +46,27 @@ namespace HomeShoppe.Controllers
             ViewBag.Total = totalRecord;
             ViewBag.Page = page;
 
+            int maxPage = 5;
+            int totalPage = 0;
+
+            totalPage = (int)Math.Ceiling((double)(totalRecord / pageSize));
+            ViewBag.TotalPage = totalPage;
+            ViewBag.MaxPage = maxPage;
+            ViewBag.First = 1;
+            ViewBag.Last = totalPage;
+            ViewBag.Next = page + 1;
+            ViewBag.Prev = page - 1;
+
+            return View(model);
+        }
+        public ActionResult Search(string keyword, int page = 1, int pageSize = 1)
+        {
+            int totalRecord = 0;
+            var model = new ProductDAO().Search(keyword, ref totalRecord, page, pageSize);
+
+            ViewBag.Total = totalRecord;
+            ViewBag.Page = page;
+            ViewBag.Keyword = keyword;
             int maxPage = 5;
             int totalPage = 0;
 
