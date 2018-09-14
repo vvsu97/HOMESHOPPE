999072635685307b553008f91cf38618f81729f2
SU VO <1512468@student.hcmus.edu.vn>
2018-09-14 18:04:11 +0700

    update tags

--- HomeShoppe/HomeShoppe/Areas/Admin/Controllers/ContentController.cs
+++ HomeShoppe/HomeShoppe/Areas/Admin/Controllers/ContentController.cs
@@ -1,21 +1,28 @@
 ﻿using Model.DAO;
 using Model.EF;
+using HomeShoppe.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HomeShoppe.Areas.Admin.Controllers;
+using HomeShoppe;
 
-namespace HomeShoppe.Areas.Admin.Controllers
+namespace OnlineShop.Areas.Admin.Controllers
 {
     public class ContentController : BaseController
     {
         // GET: Admin/Content
-        public ActionResult Index()
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
         {
-            return View();
+            var dao = new ContentDAO();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+
+            ViewBag.SearchString = searchString;
+            return View(model);
         }
-        #region method
+
         [HttpGet]
         public ActionResult Create()
         {
@@ -27,11 +34,12 @@ namespace HomeShoppe.Areas.Admin.Controllers
         public ActionResult Edit(long id)
         {
             var dao = new ContentDAO();
-            var content = dao.GetById(id);
-            SetViewBag(content.CategoryID);
-            return View();
-        }
+            var content = dao.GetByID(id);
 
+            SetViewBag(content.CategoryID);
+
+            return View(content);
+        }
 
         [HttpPost]
         public ActionResult Edit(Content model)
@@ -43,21 +51,29 @@ namespace HomeShoppe.Areas.Admin.Controllers
             SetViewBag(model.CategoryID);
             return View();
         }
+
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult Create(Content model)
         {
             if (ModelState.IsValid)
             {
-
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.CreateBy = session.UserName;
+                var culture = Session[CommonConstants.CurrentCulture];
+                model.Language = culture.ToString();
+                new ContentDAO().Create(model);
+                return RedirectToAction("Index");
             }
             SetViewBag();
             return View();
         }
-        public void SetViewBag(long ? selectedId =null)
+
+
+        public void SetViewBag(long? selectedId = null)
         {
             var dao = new CategoryDAO();
             ViewBag.CategoryID = new SelectList(dao.ListAll(), "ID", "Name", selectedId);
         }
-        #endregion
     }
 }
--- HomeShoppe/Model/DAO/ContentDAO.cs
+++ HomeShoppe/Model/DAO/ContentDAO.cs
@@ -1,25 +1,196 @@
-﻿
-using Model.EF;
+﻿using Model.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
+using PagedList;
+using System.Web;
 
 namespace Model.DAO
 {
     public class ContentDAO
     {
         HomeShoppe db = null;
-
+        public static string USER_SESSION = "USER_SESSION";
         public ContentDAO()
         {
             db = new HomeShoppe();
         }
 
-        public Content GetById(long id)
+        public IEnumerable<Content> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Content> model = db.Contents;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString) || x.Name.Contains(searchString));
+            }
+
+            return model.OrderByDescending(x => x.CreateDate).ToPagedList(page, pageSize);
+        }
+        /// <summary>
+        /// List all content for client
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public IEnumerable<Content> ListAllPaging(int page, int pageSize)
+        {
+            IQueryable<Content> model = db.Contents;
+            return model.OrderByDescending(x => x.CreateDate).ToPagedList(page, pageSize);
+        }
+        public IEnumerable<Content> ListAllByTag(string tag, int page, int pageSize)
+        {
+            var model = (from a in db.Contents
+                         join b in db.ContentTabs
+                         on a.ID equals b.ContentID
+                         where b.TabID == tag
+                         select new
+                         {
+                             Name = a.Name,
+                             MetaTitle = a.MetaTitle,
+                             Image = a.Image,
+                             Description = a.Description,
+                             CreatedDate = a.CreateDate,
+                             CreatedBy = a.CreateBy,
+                             ID = a.ID
+
+                         }).AsEnumerable().Select(x => new Content()
+                         {
+                             Name = x.Name,
+                             MetaTitle = x.MetaTitle,
+                             Image = x.Image,
+                             Description = x.Description,
+                             CreateDate = x.CreatedDate,
+                             CreateBy = x.CreatedBy,
+                             ID = x.ID
+                         });
+            return model.OrderByDescending(x => x.CreateDate).ToPagedList(page, pageSize);
+        }
+
+        public Content GetByID(long id)
         {
             return db.Contents.Find(id);
+        }
+
+        public Tab GetTag(string id)
+        {
+            return db.Tabs.Find(id);
+        }
+
+        public long Create(Content content)
+        {
+            //Xử lý alias
+            if (string.IsNullOrEmpty(content.MetaTitle))
+            {
+                content.MetaTitle = StringHelper.ToUnsignString(content.Name);
+            }
+            content.CreateDate = DateTime.Now;
+            //content.ViewCount = 0;
+            db.Contents.Add(content);
+            db.SaveChanges();
+
+            //Xử lý tag
+            if (!string.IsNullOrEmpty(content.Tabs))
+            {
+                string[] tags = content.Tabs.Split(',');
+                foreach (var tag in tags)
+                {
+                    var tagId = StringHelper.ToUnsignString(tag);
+                    var existedTag = this.CheckTag(tagId);
+
+                    //insert to to tag table
+                    if (!existedTag)
+                    {
+                        this.InsertTag(tagId, tag);
+                    }
+
+                    //insert to content tag
+                    this.InsertContentTag(content.ID, tagId);
+
+                }
+            }
+
+            return content.ID;
+        }
+        public long Edit(Content content)
+        {
+            //Xử lý alias
+            if (string.IsNullOrEmpty(content.MetaTitle))
+            {
+                content.MetaTitle = StringHelper.ToUnsignString(content.Name);
+            }
+            content.CreateDate = DateTime.Now;
+            db.SaveChanges();
+
+            //Xử lý tag
+            if (!string.IsNullOrEmpty(content.Tabs))
+            {
+                this.RemoveAllContentTag(content.ID);
+                string[] tags = content.Tabs.Split(',');
+                foreach (var tag in tags)
+                {
+                    var tagId = StringHelper.ToUnsignString(tag);
+                    var existedTag = this.CheckTag(tagId);
+
+                    //insert to to tag table
+                    if (!existedTag)
+                    {
+                        this.InsertTag(tagId, tag);
+                    }
+
+                    //insert to content tag
+                    this.InsertContentTag(content.ID, tagId);
+
+                }
+            }
+
+            return content.ID;
+        }
+        public void RemoveAllContentTag(long contentId)
+        {
+            db.ContentTabs.RemoveRange(db.ContentTabs.Where(x => x.ContentID == contentId));
+            db.SaveChanges();
+        }
+        public void InsertTag(string id, string name)
+        {
+            var tag = new Tab();
+            tag.ID = id;
+            tag.Name = name;
+            db.Tabs.Add(tag);
+            db.SaveChanges();
+        }
+
+        public void InsertContentTag(long contentId, string tagId)
+        {
+            var contentTag = new ContentTab();
+            contentTag.ContentID = contentId;
+            contentTag.TabID = tagId;
+            db.ContentTabs.Add(contentTag);
+            db.SaveChanges();
+        }
+        public bool CheckTag(string id)
+        {
+            return db.Tabs.Count(x => x.ID == id) > 0;
+        }
+
+        public List<Tab> ListTag(long contentId)
+        {
+            var model = (from a in db.Tabs
+                         join b in db.ContentTabs
+                         on a.ID equals b.TabID
+                         where b.ContentID == contentId
+                         select new
+                         {
+                             ID = b.TabID,
+                             Name = a.Name
+                         }).AsEnumerable().Select(x => new Tab()
+                         {
+                             ID = x.ID,
+                             Name = x.Name
+                         });
+            return model.ToList();
         }
     }
 }
